c1f565205fc717ee29bed45637e3f389146eee81
AnnabelM7 <annabelmaksimov@gmail.com>
2025-03-03 12:30:59 +0200

    First commit

---
+++ src/test/java/com/cgi2025/lennuplaan/CgiProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi2025.lennuplaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
